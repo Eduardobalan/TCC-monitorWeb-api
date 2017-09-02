3de6ca7fd50884b776e4d0a7bea04ccb83258862
Eduardo Balan <ebalan@azi.com.br>
2017-09-02 12:58:21 -0400

    1.0_00004 monitoramento e informações da Swap

---
+++ src/main/java/br/com/webmonitor/business/InformacoesSwapBO.java
@@ -0,0 +1,13 @@
+package br.com.webmonitor.business;
+
+import br.com.webmonitor.entity.InformacoesSwap;
+import br.com.webmonitor.repository.InformacoesSwapRepository;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Eduardo Balan on 01/07/2017.
+ */
+@Component
+public class InformacoesSwapBO extends GenericBO<InformacoesSwap, InformacoesSwapRepository> {
+
+}
